8d6c1b93e529722817bfa875b7d6a23304ede632
Leonardo <srleon19@hotmail.com>
2020-07-09 21:50:19 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,17 @@
+/*import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+*/
+
+/* intl module não funcionando - brickando a aplicação */
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
